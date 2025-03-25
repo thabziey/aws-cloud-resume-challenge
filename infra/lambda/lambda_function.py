fe5a8386a1f0335a24dd6db3b397c95164378d85
Thabziey <aloischinyoka@yahoo.com>
2025-03-25 22:22:28 +0100

    Updated lambda function and index.html

---
+++ infra/lambda/lambda_function.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+from decimal import Decimal
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('cloudresume-test')
+
+def lambda_handler(event, context):
+        response = table.get_item(Key={'id': '0'})
+        views = (response['Item']['views'])
+        views = views + 1
+
+        print(views)
+        response = table.put_item(Item={'id': '0', 'views': views})
+
+        return views
